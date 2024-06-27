443e70d4ecd4d43393b032de4b637717e40e9497
Cabezlea <Leocabezas@icloud.com>
2024-06-27 13:01:40 -0500

    Responsive design for the SignIn will implement the actual design like header

--- pages/SignIn.jsx
+++ pages/SignIn.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, Dimensions } from 'react-native';
 
+const {height} = Dimensions.get('window');
 const SignInScreen = ({ onSignIn }) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Welcome to the App Norero</Text>
+    <View style={styles.container}>
+      <Text style={styles.welcomeText}>Welcome to the App Norero</Text>
       <Button title="Sign In" onPress={onSignIn} />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    backgroundColor: '#f0e5de',
+    paddingTop: height * 0.13, // 13% of the screen to make sure it is responsive
+  },
+  welcomeText: {
+    fontSize: 18,
+    marginBottom: 20, // Add some space before the button
+  },
+});
 
 export default SignInScreen;
